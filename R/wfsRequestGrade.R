3b77467aa4e6b013b7dbdff2e19bd456a9a328c0
jcmartinssh <arq.joaquim@gmail.com>
2023-07-18 01:07:34 -0300

    inicio script WFS R

--- R/wfsRequestGrade.R
+++ R/wfsRequestGrade.R
@@ -1,1 +1,21 @@
-library(sf)
+library(sf) # simple features packages for handling vector GIS data
+library(httr) # generic webservice package
+library(tidyverse) # a suite of packages for data wrangling, transformation, plotting, ...
+library(ows4R) # interface for OGC webservices
+
+wfs_bwk <- "https://geoservicos.ibge.gov.br/geoserver/ows"
+
+url <- parse_url(wfs_bwk)
+
+url$query <- list(service = "wfs",
+                  version = "2.0.0",
+                  request = "GetCapabilities"
+                  )
+
+request <- build_url(url)
+
+request
+
+bwk_client <- WFSClient$new(wfs_bwk,
+                            serviceVersion = "2.0.0")
+
