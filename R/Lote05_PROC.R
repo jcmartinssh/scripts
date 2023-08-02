0cc8823913153e1272c3b72dc4d46f5b43eb4dab
jcmartinssh <arq.joaquim@gmail.com>
2023-08-02 14:44:35 -0300

    finalizacao script faces cnefe

--- R/Lote05_PROC.R
+++ R/Lote05_PROC.R
@@ -12,27 +12,33 @@ library(readr)
 ## ela utiliza algumas bibliotecas consagradas, como o GDAL, e é útil até para abrir arquivos de formatos variados
 library(sf)
 
-## LIGA / DESLIGA o modo de teste (TRUE ou FALSE)
-teste <- TRUE
+## carrega a biblioteca de API do Rstudio para utilizar os diálogos de seleçào de arquivo
+library(rstudioapi)
 
+# define o modo de teste
+teste = FALSE
 ## roda o comando readr::fwf_empty para detectar automaticamente as quebras das colunas a partir de um único arquivo.
 ## esse arquivo deve garantir a existência de pelo menos um valor em cada coluna. Não utilize o Distrito Federal.
-## o parâmetro do comando é o endereço e nome do arquivo
-scanfwf <- fwf_empty("C:\\ACELERADOR\\Lote5\\Arquivos_DI\\V_gQF_SUDESTE (ES-RJ).csv")
+scanfwf <- fwf_empty(selectFile(caption = "Selecionar arquivo com conteúdo típico", filter = "CSV files (*.csv)"))
 
 ## endereço da pasta raiz onde estão as subpastas com os arquivos de entrada e de saída
-pastaRaiz <- ifelse(teste == TRUE, "C:\\ACELERADOR\\Lote5\\teste\\", "C:\\ACELERADOR\\Lote5\\")
+# pastaRaiz <- ifelse(teste == TRUE, "C:\\ACELERADOR\\Lote5\\teste\\", "C:\\ACELERADOR\\Lote5\\")
+pasta_entrada <- selectDirectory()
 
 ## cria a lista de arquivos lendo o conteúdo da pasta dos arquivos originais.
 ## o parâmetro pattern permite definir um string para restringir os arquivos de interesse.
 ## nesse caso, todos os arquivos de interesse, e somente eles, devem conter .csv no nome
-lista_arq <- list.files(paste(pastaRaiz, "Arquivos_DI\\", sep = ""), pattern = ".csv")
+lista_arq <- list.files(paste(pasta_entrada, sep = ""), pattern = ".csv")
+
+lista_arq[1:3]
+## reduz a lista de arquivos de entrada para rodar o teste
+if(teste == TRUE) {
+  lista_arq <- lista_arq[1:2]
+}
 
 ## endereço do geopackage onde serão salvos as camadas e tabelas
-arquivo_saida <- paste(pastaRaiz, "_GPKG\\lote5_faces.gpkg", sep = "" )
-
-## reduz a lista de arquivos de entrada para rodar o teste
-lista_arq <- ifelse(teste == TRUE, lista_arq[c(1, 2)], lista_arq)
+# arquivo_saida <- paste(pastaRaiz, "_GPKG\\lote5_faces.gpkg", sep = "" )
+arquivo_saida <- selectFile(caption = "Arquivo de saída - geopackage", label = "Save", filter = "GPKG files (*.gpkg)")
 
 ## cria lista vazia pra receber numero de linhas de cada tabela processada
 lista_total <- list()
@@ -40,24 +46,35 @@ lista_total <- list()
 ## loop de processamento dos arquivos da DI
 for(i in lista_arq) {
   
-  # pega o nome do arquivo da lista
-  nome_arq <- i
-  
   # constrói o endereço do arquivo de entrada
-  arquivo_entrada <- paste(pastaRaiz, "Arquivos_DI\\", nome_arq, sep = "")
+  arquivo_entrada <- paste(pasta_entrada, "//", i, sep = "")
   
   # lê o arquivo da DI de largura fixa, utilizando os parâmetros computados anteriormente
   # define os tipos das colunas usando a notação compacta: c = character; n = numeric
   tabela_faces <- read_fwf(arquivo_entrada, col_positions = scanfwf, col_types = "cccccccnnnnnnnnnnnnnnnnnnnnnnnn")
   
+  # adiciona uma coluna com o nome do arquivo de origem para controle
+  tabela_faces$origem <- i
+  
+  # adiciona o item com o nome do arquivo e total de faces na tabela para controle
+  lista_total[[i]] <- nrow(tabela_faces)
+  
   # acrescenta a tabela na camada indicada dentro do gpkg, sem apagar o que já existe
   write_sf(tabela_faces, arquivo_saida, layer = "dados_faces_lote5", append = TRUE)
   
 }
-                  
+
+## adiciona item total com valor do somatório dos items dos arquivos
+lista_total[["total"]] <- sum(unlist(lista_total, use.names = FALSE))
+
 ## conta o número de feições na camada produzida usando SQL
-verificacao <- read_sf(arquivo_saida,
-                       query = "SELECT count(*) FROM dados_faces_lote5")
+ver_geral <- read_sf(arquivo_saida,
+                       query = "SELECT count(*) as total FROM dados_faces_lote5")
+
+
+## conta o número de feições para cada arquivo na camada produzida usando SQL
+ver_arq <- read_sf(arquivo_saida,
+                       query = "SELECT origem, count(*) as total FROM dados_faces_lote5 GROUP BY origem")
 
 ## carrega os primeiros 50 elementos da camada produzida para verificação
 columns <- read_sf(arquivo_saida,
@@ -65,6 +82,5 @@ columns <- read_sf(arquivo_saida,
 
 ## cria a lista de Municípios a partir do geocódigo das faces do arquivo
 lista_mun <- read_sf(arquivo_saida,
-               query = "SELECT DISTINCT SUBSTR(X1, 1, 7) FROM dados_faces_lote5")
- 
+               query = "SELECT DISTINCT SUBSTR(X1, 1, 7) as geomun, count(*) as total_faces FROM dados_faces_lote5 GROUP BY geomun")
 
--- R/wfsRequestGrade.R
+++ R/wfsRequestGrade.R
@@ -3,6 +3,21 @@ library(httr) # generic webservice package
 library(tidyverse) # a suite of packages for data wrangling, transformation, plotting, ...
 library(ows4R) # interface for OGC webservices
 
+pr_albers_br <- 'PROJCS["Conica_Equivalente_de_Albers_Brasil",
+                         GEOGCS["GCS_SIRGAS2000",
+                                DATUM["D_SIRGAS2000",
+                                      SPHEROID["Geodetic_Reference_System_of_1980",6378137,298.2572221009113]],
+                                PRIMEM["Greenwich",0],
+                                UNIT["Degree",0.017453292519943295]],
+                         PROJECTION["Albers"],
+                         PARAMETER["standard_parallel_1",-2],
+                         PARAMETER["standard_parallel_2",-22],
+                         PARAMETER["latitude_of_origin",-12],
+                         PARAMETER["central_meridian",-54],
+                         PARAMETER["false_easting",5000000],
+                         PARAMETER["false_northing",10000000],
+                         UNIT["Meter",1]]'
+
 wfs_bwk <- "https://geoservicos.ibge.gov.br/geoserver/ows"
 
 url <- parse_url(wfs_bwk)
@@ -16,6 +31,6 @@ request <- build_url(url)
 
 request
 
-bwk_client <- WFSClient$new(wfs_bwk,
-                            serviceVersion = "2.0.0")
+bwk_client <- WFSCapabilities$new(wfs_bwk,
+                            version = "1.1.0")
 
